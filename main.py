639832e1115937ae24972a6e0620045758097009
RodgerDodg3r <bartoszszczepkowski99@gmaill.com>
2025-06-30 21:56:48 +0100

    Finnal Update

--- main.py
+++ main.py
@@ -28,27 +28,22 @@ def get_loading_plan(path):
 
 def get_loading_plan_headers(content):
     for row in content:
-        if (row[0] == "Vehicle No."):
+        if (row[3] == "Vehicle No."):
             return row
-
-
-file_content = get_loading_plan('load_plan.csv')
-headers = get_loading_plan_headers(file_content)
-
 
 
 def create_tracker_data():
 
     loads = {}
-
+    pallet_number = 0
     for index, row in enumerate(file_content):
+        
         if (row[get_loading_plan_headers(file_content).index('Customer Depot')] == "TOTAL"):
             pallet_number = row[get_loading_plan_headers(file_content).index('MORAN')]
-            continue
-        if (index < 3):
+            print(pallet_number)
             continue
     
-        
+        print(pallet_number)
         load_number = row[get_loading_plan_headers(file_content).index('Vehicle No.')]
         load_name = row[get_loading_plan_headers(file_content).index('Customer Depot')]
         departure_time = row[get_loading_plan_headers(file_content).index('Time of Departure')]
@@ -76,19 +71,26 @@ def create_csv_tracker(loads):
         depos = loads[load]["depots"]
         depos = ' / '.join(depos)
 
+        depos = depos[:-3]
+
         load_number = load
         departure_time = loads[load]["departure_time"]
         haulier = loads[load]["haulier"]
         pallet_number = loads[load]["pallet_number"]
-
         file_line = f",{load_number}, {departure_time}, {depos}, {haulier},,,{pallet_number}\n"
+
+        if (load_number == ""):
+            continue
+        if (depos == " " or depos == ""):
+            continue
+
         lines.append(file_line)
 
     with open('tracker.csv', 'w') as file:
         file.writelines(lines)
     
 
-
-get_loading_plan('load_plan.csv')
-get_loading_plan_headers(file_content)
+file_content = get_loading_plan('load_plan.csv')
+headers = get_loading_plan_headers(file_content)
+print(headers)
 create_csv_tracker(create_tracker_data())
